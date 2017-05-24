5f167a6ff730a52d73c30e5773550a099fcb0f42
Anna Fominova <anna.spectr@gmail.com>
2017-05-24 14:15:03 +0000

    Final

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $("section").hide();
+
+    $("#home1").click(function(){
+         $("#home").toggle(3000);
+    });
+    $("#mercuri1").click(function(){
+         $("#mercuri").toggle(3000);
+    });
+    $("#venus1").click(function(){
+         $("#venus").toggle(3000);
+    });
+    $("#earth1").click(function(){
+         $("#earth").toggle(3000);
+    });
+    $("#mars1").click(function(){
+         $("#mars").toggle(3000);
+    });
+    $("#jupiter1").click(function(){
+         $("#jupiter").toggle(3000);
+    });
+    $("#saturn1").click(function(){
+         $("#saturn").toggle(3000);
+    });
+    $("#uranus1").click(function(){
+         $("#uranus").toggle(3000);
+    });
+    $("#neptune1").click(function(){
+         $("#neptune").toggle(3000);
+    });
+    $("#pluto1").click(function(){
+         $("#pluto").toggle(3000);
+    });
+});
